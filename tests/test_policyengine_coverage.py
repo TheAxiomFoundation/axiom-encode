10603983090b57d5024668c36553e357e3433a56
Max Ghenis <mghenis@gmail.com>
2026-05-25 01:50:15 -0400

    Classify 3121(n) as PE not comparable

--- tests/test_policyengine_coverage.py
+++ tests/test_policyengine_coverage.py
@@ -594,6 +594,11 @@ rules:
             "foreign_affiliate",
         ),
         (
+            "statutes/26/3121/n.yaml",
+            "us:statutes/26/3121/n#member_of_uniformed_service",
+            "member_of_uniformed_service",
+        ),
+        (
             "statutes/26/3121/y.yaml",
             "us:statutes/26/3121/y#transferred_federal_employee_international_organization_service_is_employment",
             "transferred_federal_employee_international_organization_service_is_employment",
